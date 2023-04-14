01f7bf879709044f69da704eaa564eb17f4a1a7a
Joo JIn Soo <argentdarae@gmail.com>
2023-04-15 00:13:06 +0900

    [Feat] 레이저 장애물 게임 오브젝트 구성

--- Katana_ZERO/Assets/Scripts/Object/LaserScript.cs
+++ Katana_ZERO/Assets/Scripts/Object/LaserScript.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private LineRenderer _line;
+
+    private void Awake()
     {
-        
+        _line = GetComponentInChildren<LineRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
 }
