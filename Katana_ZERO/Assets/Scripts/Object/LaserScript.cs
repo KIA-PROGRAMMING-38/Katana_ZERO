c035f9a71bc66e20272c204eb0ae63c470347a50
Joo JIn Soo <argentdarae@gmail.com>
2023-04-15 00:50:27 +0900

    [Feat] 레이저 장애물 코드 구현

--- Katana_ZERO/Assets/Scripts/Object/LaserScript.cs
+++ Katana_ZERO/Assets/Scripts/Object/LaserScript.cs
@@ -1,16 +1,28 @@
+using LiteralRepository;
 using UnityEngine;
 
 public class LaserScript : MonoBehaviour
 {
-    private LineRenderer _line;
+    private ParticleSystem _laserParticle;
 
     private void Awake()
     {
-        _line = GetComponentInChildren<LineRenderer>();
+        _laserParticle = GetComponentInChildren<ParticleSystem>();
+        _laserParticle.Play();
     }
 
-    private void Update()
+    private void OnTriggerEnter2D( Collider2D collision )
     {
+        if ( collision.gameObject.layer == LayerMaskNumber.s_ColliderSensor )
+        {
+            collision.gameObject.transform.root.SendMessage( FuncLiteral.OnDamaged );
+        }
+    }
 
+    private void OnTriggerStay2D( Collider2D collision )
+    {
+        if ( collision != null )
+        {
+        }
     }
 }
