6b1d8fbda498c68b482d4c41a1633c15a8d53624
Joo JIn Soo <argentdarae@gmail.com>
2023-04-17 21:38:15 +0900

    [Feat] 슬로우 모션 발동시 화면 이펙트 구현

--- Katana_ZERO/Assets/Scripts/System/TimeManager.cs
+++ Katana_ZERO/Assets/Scripts/System/TimeManager.cs
@@ -1,17 +1,83 @@
+using System.Collections;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    void Update()
+    [SerializeField]
+    private GameObject _slowTimeEffectPanel;
+    [SerializeField]
+    private CanvasGroup _cg;
+    [SerializeField][Range(0f, 1f)]
+    private float _duration = 0.15f;
+
+    private float _elapsedTime;
+    private IEnumerator _initialCoroutine;
+    private IEnumerator _endCoroutine;
+
+    private void Update()
     {
         if ( Input.GetKeyUp( KeyCode.LeftShift ) )
         {
             Time.timeScale = 1f;
+
+            EndCoroutine();
         }
 
-        if ( Input.GetKey( KeyCode.LeftShift ) )
+        if ( Input.GetKeyDown( KeyCode.LeftShift ))
         {
-            Time.timeScale = 0.2f;
+            StartCouroutine();
+        } 
+    }
+
+    private void StartCouroutine()
+    {
+        if ( null != _initialCoroutine )
+        {
+            StopCoroutine( _endCoroutine );
+        }
+
+        _initialCoroutine = ElapsedTime();
+        StartCoroutine( _initialCoroutine );
+    }
+
+    private void EndCoroutine()
+    {
+        if ( null != _endCoroutine )
+        {
+            StopCoroutine( _initialCoroutine );
+        }
+
+        _endCoroutine = EscapeTime();
+        StartCoroutine( _endCoroutine );
+    }
+
+    private IEnumerator ElapsedTime()
+    {
+        Time.timeScale = 0.2f;
+        _elapsedTime = 0f;
+
+        while ( _cg.alpha < 1f )
+        {
+            _cg.alpha = Mathf.Lerp( 0f, 1f, _elapsedTime / _duration );
+            _elapsedTime += Time.unscaledDeltaTime;
+
+            yield return null;
         }
     }
+
+    private IEnumerator EscapeTime()
+    {
+        _elapsedTime = 0f;
+
+        while ( _cg.alpha >= 0f )
+        {
+            _cg.alpha = Mathf.Lerp( 1f, 0f, _elapsedTime / _duration );
+            _elapsedTime += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+    }
+
 }
+
+
